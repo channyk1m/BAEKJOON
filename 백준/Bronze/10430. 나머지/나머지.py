ddad06bcb9838ac7e52174c71970f8ea72ccebb7
channyk1m <124776769+channyk1m@users.noreply.github.com>
2024-07-28 14:18:51 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,8 @@
+s = input().split()
+s[0] = int(s[0])
+s[1] = int(s[1])
+s[2] = int(s[2])
+print((s[0] + s[1]) % s[2])
+print(((s[0] % s[2]) + (s[1] % s[2])) % s[2])
+print((s[0] * s[1]) % s[2])
+print(((s[0] % s[2]) * (s[1] % s[2])) % s[2])
