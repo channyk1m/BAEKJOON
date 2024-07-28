d59952eb4c0c06dee9507d33af3d710ba9ca128a
channyk1m <124776769+channyk1m@users.noreply.github.com>
2024-07-28 14:20:31 +0900

    [Bronze V] Title: A+B - 8, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+x = int(input())
+
+for i in range(x):
+    y = input().split()
+    a = int(y[0])
+    b = int(y[1])
+    print("Case #" + str(i + 1) +": "+ str(a) + " + " + str(b) + " = "+ str(a + b))
