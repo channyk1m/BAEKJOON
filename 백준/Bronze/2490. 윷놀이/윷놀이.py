79281f7a36572c1e84b0a6b7fb445251a3b87f2b
channyk1m <124776769+channyk1m@users.noreply.github.com>
2024-07-28 14:15:03 +0900

    [Bronze III] Title: 윷놀이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,59 @@
+x = input().split()
+
+a = int(x[0])
+b = int(x[1])
+c = int(x[2])
+d = int(x[3])
+
+n = a + b + c + d
+
+if n == 4:
+    print('E')
+elif n == 3:
+    print('A')
+elif n == 2:
+    print('B')
+elif n == 1:
+    print('C')
+else:
+    print('D')
+    
+x = input().split()
+
+a = int(x[0])
+b = int(x[1])
+c = int(x[2])
+d = int(x[3])
+
+n = a + b + c + d
+
+if n == 4:
+    print('E')
+elif n == 3:
+    print('A')
+elif n == 2:
+    print('B')
+elif n == 1:
+    print('C')
+else:
+    print('D')
+    
+x = input().split()
+
+a = int(x[0])
+b = int(x[1])
+c = int(x[2])
+d = int(x[3])
+
+n = a + b + c + d
+
+if n == 4:
+    print('E')
+elif n == 3:
+    print('A')
+elif n == 2:
+    print('B')
+elif n == 1:
+    print('C')
+else:
+    print('D')
