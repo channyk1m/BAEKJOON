2f17e69cd9c2eda35c1e9096a468577420d0722d
channyk1m <124776769+channyk1m@users.noreply.github.com>
2024-12-08 15:01:51 +0900

    Update 별 찍기 － 2.py

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,7 +1,17 @@
+# N = int(input())
+# a = 1
+# for i in range(N):
+#     print(' ' * (N - 1), end="")    # 연산햇갈리지않기
+#     print('*' * a)
+#     N = N - 1
+#     a = a + 1
+
+
+
 N = int(input())
-a = 1
-for i in range(N):
-    print(' ' * (N - 1), end="")    # 연산햇갈리지않기
-    print('*' * a)
-    N = N - 1
-    a = a + 1
+for i in range(N, 0, -1):           # 줄 수
+    for j in range(0, i - 1, 1):    # 공백을 한 줄에 찍기
+        print(' ', end='')
+    for k in range(i - 1, N, 1):    # 별을 한 줄에 찍기
+        print('*', end='')
+    print()                         # end=''을 쓰면 꼭 print()를 해라
